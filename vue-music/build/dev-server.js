a1886760dcaf8e8dc27cecde8fb52c66905a1994
showay-yq <yuqian@ishoway.com>
2017-09-14 11:39:05 +0800

    '歌单列表通过服务端代理的方式绕过referer限制实现前端数据抓取'

--- vue-music/build/dev-server.js
+++ vue-music/build/dev-server.js
@@ -1,3 +1,4 @@
+// node js开启的dev-server
 require('./check-versions')()
 
 var config = require('../config')
@@ -11,6 +12,7 @@ var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
+var axios = require('axios')  // Promise based HTTP client for the browser and node.js
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
@@ -21,6 +23,32 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+// 手动服务端代理开始
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function (req, res) { // 自定义方法getDiscList
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'  // 本地服务端请求真实服务器地址(代理) <绕过host和referer限制>
+
+  axios.get(url, {  // 发送http请求
+    headers: {
+      referer: 'https://c.y.qq.com/', // 修改referer host成为qq相关的
+      host: 'c.y.qq.com'
+    },
+    params: req.query // 浏览器ajax请求本地server接口的时候带的参数原封不动传给http请求 附在qq服务端url地址后面
+  }).then((response) => {
+    res.json(response.data) // 本地server把服务端响应数据吐给浏览器端 => 前端接收到数据
+    // res是本地server的response   response是qq接口给我们返回的response
+  }).catch((e) => {
+    'use strict'
+    console.log(e)
+  })
+})
+
+app.use('/api', apiRoutes)  // Use the given middleware function, with optional mount path, defaulting to "/".
+// 这就拼成了/api/getDiscList的本地服务器地址供前端访问
+// 服务端代理结束
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -35,7 +63,7 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    hotMiddleware.publish({action: 'reload'})
     cb()
   })
 })
@@ -44,7 +72,7 @@ compiler.plugin('compilation', function (compilation) {
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {target: options}
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
--- vue-music/src/api/recommend.js
+++ vue-music/src/api/recommend.js
@@ -1,6 +1,7 @@
 // 获取推荐数据
 import jsonp from 'common/js/jsonp' // 该js模块把jsonp这一个函数作为default暴露出来 所以无需加{}
 import {commonParams, options} from './config'  // 引入公用数据
+import axios from 'axios' // vue提供的官方ajax库 浏览器端发送xhr请求 nodejs发送http request 二者语法一样 支持promise API
 
 export function getRecommend () { // 获取轮播图数据
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
@@ -14,8 +15,10 @@ export function getRecommend () { // 获取轮播图数据
   return jsonp(url, data, options)
 }
 
-export function getDiscList () {  // 获取歌单数据
-  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+export function getDiscList () {  // 获取歌单数据 (前端部分) qq服务器有host与referer的限制 而前端不能直接去修改request header
+  const url = '/api/getDiscList'  // 不直接请求qq服务端而请求本地server端 不再是jsonp请求 要改成ajax请求
+  // 设置referer可以防止用户直接通过浏览器抓接口
+  // 我们的做法是欺骗qq服务端 使其认为是网站发送的请求 其实是我们这的前端发送的(后端代理发送http请求,可以修改referer和host)
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
@@ -25,8 +28,13 @@ export function getDiscList () {  // 获取歌单数据
     sortId: 5,
     needNewCode: 0,
     categoryId: 10000000,
-    rnd: Math.random()
+    rnd: Math.random(),
+    format: 'json'  // ajax请求需要json格式的数据
   })
 
-  return jsonp(url, data, options)
+  return axios.get(url, {
+    params: data  // 这个参数也支持?&的写法
+  }).then((res) => {
+    return Promise.resolve(res.data)  // 返回一个promise对象  recommend.vue里调用
+  })
 }
