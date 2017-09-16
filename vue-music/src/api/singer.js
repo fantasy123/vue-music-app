962883ea593e592e4288590cb12d60f430c05cae
BYSJ <bourgeois_123@163.com>
2017-09-16 21:31:47 +0800

    '歌手列表数据抓取'

---
+++ vue-music/src/api/singer.js
@@ -0,0 +1,19 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all', // 查找所有歌手
+    pagesize: 100,  // 一个请求能查100条数据
+    pagenum: 1, // 查第一页数据
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq'
+  })
+
+  return jsonp(url, data, options)
+}
--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>歌手路由</div>
+  <div class="singer"></div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
 
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
