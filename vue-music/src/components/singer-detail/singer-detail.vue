0a7bf39ed056280db77466c2d0bc925e7cd92107
showay-yq <yuqian@ishoway.com>
2017-10-10 10:16:48 +0800

    '完成歌手详情数据的抓取'

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -7,6 +7,8 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex' // 取数据语法糖
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
 
   export default {
     computed: { // 扩展到计算属性里
@@ -15,7 +17,24 @@
       ])
     },
     created() {
-      console.log(this.singer)  // 通过vuex解决路由之间数据传递的问题
+//      console.log(this.singer)  // 通过vuex解决路由之间数据传递的问题
+      this._getDetail() // 详情页加载即获取歌手详情数据
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {  // 只有点击列表跳到详情 vuex才会set这个singer 才能取到id
+          // 在详情页刷新时 singer是一个空对象 没有id 路由回退到列表
+          this.$router.push('/singer')
+
+          return
+        }
+
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)  // 长度为100的数组
+          }
+        })
+      }
     }
   }
 </script>
