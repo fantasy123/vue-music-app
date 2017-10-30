52045ce6ff5102e0991c2d8e057ff015771eae1d
showay-yq <yuqian@ishoway.com>
2017-10-30 16:11:19 +0800

    '抓取排行榜数据'

--- vue-music/src/components/rank/rank.vue
+++ vue-music/src/components/rank/rank.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>排行路由</div>
+  <div class="rank">
+    <div class="toplist">
+      <ul>
+        <li class="item">
+          <div class="icon">
+            <img width="100" height="100">
+          </div>
+          <ul class="songlist">
+            <li class="song">
+              <span></span>
+              <span></span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {getTopList} from 'api/rank'
+  import {ERR_OK} from 'api/config'
 
+  export default {
+    created() {
+      this._getTopList()
+    },
+    methods: {
+      _getTopList() {
+        getTopList().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.topList)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .toplist
+      height: 100%
+      overflow: hidden
+      .item
+        display: flex
 </style>
