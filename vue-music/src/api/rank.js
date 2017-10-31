fae9b26e23da85eb5a91cd0c65062905ef55e202
showay-yq <yuqian@ishoway.com>
2017-10-31 11:59:13 +0800

    'topList歌单数据抓取,music-list头图更改'

--- vue-music/src/api/rank.js
+++ vue-music/src/api/rank.js
@@ -12,3 +12,18 @@ export function getTopList () {
 
   return jsonp(url, data, options)
 }
+
+export function getMusicList (topid) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    topid,
+    platform: 'h5',
+    needNewCode: 1,
+    page: 'detail',
+    type: 'top',
+    tpl: 3
+  })
+
+  return jsonp(url, data, options)
+}
--- vue-music/src/components/top-list/top-list.vue
+++ vue-music/src/components/top-list/top-list.vue
@@ -1,26 +1,72 @@
 <!--与singer-detail和disc级别相同-->
 <template>
   <transition name="slide">
-    <music-list :title="title" :bgImage="bgImage"></music-list>
+    <music-list :title="title" :bgImage="bgImage" :songs="songs"></music-list>
     <!--所有复用music-list组件的父组件都要传入这2个数据:标题和头图-->
+    <!--songs额外抓取-->
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex' // 拿selectItem设置的数据
+  import {ERR_OK} from 'api/config'
+  import {getMusicList} from 'api/rank'
+  import {createSong} from 'common/js/song'
 
   export default {
+    data() {
+      return {
+        songs: [] // 定义songs接收歌曲数据
+      }
+    },
     computed: {
       title() {
         return this.topList.topTitle
       },
       bgImage() {
-        return this.topList.picUrl
+        if (this.songs.length) {
+          return this.songs[0].image  // image是creatSong处理后Song实例的字段
+        }
+
+        return ''
       },
       ...mapGetters([
         'topList' // 特定id的排行榜对象 通过计算属性来取数据
       ])
+    },
+    created() {
+      this._getMusicList()
+    },
+    methods: {
+      _getMusicList() {
+        if (!this.topList.id) {
+          this.$router.push({
+            path: '/rank'
+          })
+
+          return
+        }
+        
+        getMusicList(this.topList.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+
+        list.forEach((item) => { // 遍历songlist 转化成Song的实例
+          const musicData = item.data
+
+          if (musicData.songid && musicData.albumid) {
+            ret.push(createSong(musicData))
+          }
+        })
+
+        return ret
+      }
     },
     components: {
       MusicList
